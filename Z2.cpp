42200cead9d1dd6d4d1fecbb338e6dbc7d5e49b3
RomaniusRedCash <124779338+RomaniusRedCash@users.noreply.github.com>
2024-09-05 08:55:17 +0300

    05.09.2024

---
+++ Z2.cpp
@@ -0,0 +1,55 @@
+/*я—»√ћјя –”“ќ…я—»√ћј
+* «адание:322,321 x^8 + 32,432 x^7 Ц x
+*			 9,09 x^6 + 6543,3 x^3 Ц 0,2132 x
+* јвтор: –едкокаша –оман ёрьевич				  ¬ерси€:1.1.1
+* √руппа: 4352
+* ƒаты: начало: 04.09.2024 - конец:потом
+*/
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+	cout.setf(ios::fixed);
+	cout.precision(7);
+	setlocale(0, "RUSSIAN");
+
+	double X;
+	cout << setiosflags(ios::left) << setw(20)
+		<< "¬ведите X: ";
+	cin >> X;
+
+	cout << "\nѕример є1" << endl;
+
+	double P = 322.321 * X + 32.432;
+
+	double Xn = X * X * X; // ^3
+	Xn *= Xn; // X^6
+	P = P * Xn - 1;
+
+	P *= X;
+
+	cout << setiosflags(ios::left) << setw(20)
+		<< "322,321X ^ 8 + 32,432X ^ 7 - X = " << P << endl;
+
+
+	/////////////////////////////////////////////////////////////
+	cout << "\n////////////////////////\n\n" << "ѕример є2" << endl;
+	/////////////////////////////////////////////////////////////
+
+	Xn = X * X;
+	P = 9.09 * Xn * X + 6543.3;
+
+	P = P * Xn - 0.2132;
+
+	P = P * X;
+
+	cout << setiosflags(ios::left) << setw(20)
+		<< "9,09X^6 + 6543,3X^3 - 0,2132X = " << P << endl;
+
+	system("pause");
+
+	return 0;
+}
