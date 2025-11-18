ac247e15b77eddc970d71f6835877ad2b75985d2
RomaniusRedCash <124779338+RomaniusRedCash@users.noreply.github.com>
2025-11-18 22:39:31 +0300

    Revert "лаба 2 на C++"
    This reverts commit 567e60d97d4960fec4088f79a3e2a2599a8a4688.

--- OOP/Lab2/src/main/java/org/example/Dater.java
+++ OOP/Lab2/src/main/java/org/example/Dater.java
@@ -1,43 +1,60 @@
-package org.example;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-class FloorData {
-    public Map<Integer, Integer> data = new HashMap<>();
-    public void add(Integer floor) {
-        data.merge(floor, 1, Integer::sum);
+class HomeStr {
+    public int floors;
+    public int repeats = 0;
+
+    public HomeStr(int floors) {
+        this.floors = floors;
+    }
+
+    public HomeStr(int floors, int repeats) {
+        this.floors = floors;
+        this.repeats = repeats;
     }
 }
 
-class HomeData {
-    public Map<Integer, FloorData> data = new HashMap<>();
-    public void add(Integer home, Integer floor) {
-        data.computeIfAbsent(home, k -> new FloorData()).add(floor);
+class City {
+    public Map<String, Map<Integer, HomeStr>> subData = new HashMap<>();
+    public Map<Integer, Integer> floorsMap = new HashMap<>();
+
+    public Map<Integer, HomeStr> get(String street) {
+        return subData.getOrDefault(street, new HashMap<>());
+    }
+
+    public Map<Integer, HomeStr> getOrCreate(String street) {
+        return subData.computeIfAbsent(street, k -> new HashMap<>());
     }
 }
 
-class StreetData {
-    public Map<String, HomeData> data = new HashMap<>();
-    public void add(String street, Integer home, Integer floor) {
-        data.computeIfAbsent(street, k -> new HomeData()).add(home, floor);
-    }
-}
+public class Dater {
+    public Map<String, City> data = new HashMap<>();
 
-class SubCityData {
-    public StreetData streets = new StreetData();
-    public Map<Integer, Integer> floors = new HashMap();
-    public void add(String street, Integer home, Integer floor) {
-        streets.add(street, home, floor);
-        floors.merge(floor, 1, Integer::sum);
+    public City get(String city) {
+        return data.getOrDefault(city, new City());
     }
-}
 
-class CityData {
-    public Map<String, SubCityData> data = new HashMap<>();
-    public void add(String city, String street, Integer home, Integer floor) {
-        data.computeIfAbsent(city, k -> new SubCityData()).add(street, home, floor);
+    public City getOrCreate(String city) {
+        return data.computeIfAbsent(city, k -> new City());
+    }
+
+    public void add(String city, String street, int house, int floors) {
+        City cityObj = getOrCreate(city);
+        Map<Integer, HomeStr> streetData = cityObj.getOrCreate(street);
+
+        HomeStr houseStr = streetData.get(house);
+        if (houseStr == null) {
+            houseStr = new HomeStr(floors);
+            streetData.put(house, houseStr);
+
+            // Обновляем floorsMap
+            cityObj.floorsMap.put(floors, cityObj.floorsMap.getOrDefault(floors, 0) + 1);
+        }
+        /* else if (houseStr.floors != floors) {
+            throw new RuntimeException("Два дома с одинаковым адресом");
+        } */
+
+        houseStr.repeats++;
     }
 }
--- OOP/Lab2/src/main/java/org/example/XmlParcer.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import java.io.File;
-
-public class XmlParcer {
-    private File file;
-
-}
