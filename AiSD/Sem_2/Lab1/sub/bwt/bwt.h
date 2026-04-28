12b00719795ca4a60a4ccd13a56e70e7eb84250d
RomaniusRedCash <romaniusredcash@PMC_Roman_Void.localdomain>
2026-04-28 08:30:12 +0300

    bwt

--- AiSD/Sem_2/Lab1/sub/bwt/bwt.h
+++ AiSD/Sem_2/Lab1/sub/bwt/bwt.h
@@ -7,14 +7,19 @@
 #include <algorithm>
 #include <cstdint>
 #include <map>
+#include <unordered_map>
+#include <deque>
 #include "../logger/logger.h"
 
 namespace bwt_ns {
-    std::string bwt_1(const std::string& str, const uint8_t& num_byte);
-    std::vector<std::string> bwt_sort(const std::string& s_bwt, const uint8_t& pos_main);
-    std::string de_bwt_1(const std::string& str, const uint8_t& num_byte);
+    std::string bwt_1(const std::string& str);
+    std::string de_bwt_1(const std::string& s_bwt, const uint8_t& bwt_pos);
+    void bwt_sort_good(std::vector<std::string>*& pv_pull1, std::vector<std::string>*& pv_pull2, const std::vector<uint8_t>& v_subpull);
+    std::string de_bwt_2(const std::string& s_bwt, const uint8_t& bwt_pos);
+    std::string de_bwt_3(const std::string& s_bwt, const size_t& bwt_pos);
+    std::string de_bwt_0(const std::string& s_bwt);
 }
 
-void bwt(std::istream& stream_in, std::ostream& stream_out, const uint8_t& num_byte);
+void bwt(std::istream& stream_in, std::ostream& stream_out);
 
-void de_bwt(std::istream& stream_in, std::ostream& stream_out, const uint8_t& num_byte);
+void de_bwt(std::istream& stream_in, std::ostream& stream_out);
