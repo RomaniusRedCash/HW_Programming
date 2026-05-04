68e8398301fd9954826b91a7084567fe2201e9a3
RomaniusRedCash <romaniusredcash@PMC_Roman_Void.localdomain>
2026-05-04 08:48:49 +0300

    убейте меня lzw

--- AiSD/Sem_2/Lab1/sub/lz/lzw/lzw.h
+++ AiSD/Sem_2/Lab1/sub/lz/lzw/lzw.h
@@ -2,18 +2,19 @@
 
 #include <cstdint>
 #include <iostream>
+#include <limits>
 #include <string>
 #include <cstring>
 #include <sstream>
 #include <string_view>
 #include <vector>
 #include <unordered_map>
-#include <set>
 #include <algorithm>
 #include "../../logger/logger.h"
 #include "../../bytebitstream.h"
 
 extern uint8_t num_byte;
+extern size_t window_buffer_size;
 
 namespace lzw_ns {
     struct node {
@@ -24,13 +25,13 @@ namespace lzw_ns {
     std::unordered_map<std::string, size_t> create_mapslovar_byte(const size_t& start = 0, size_t const& stop = 1 << (8 * num_byte));
     sstrtobb& operator<<(sstrtobb& os, node n);
     sstrtobb& operator>>(sstrtobb& is, node& n);
-    sstrtobb lzw_0(const std::string& str, const size_t& buffer_size, std::unordered_map<std::string, size_t> m_slovar);
-    std::string de_lzw_0(sstrtobb& ssbb, const size_t& buffer_size, std::vector<std::string> v_slovar);
-    std::string lzw_1(const std::string& str, const size_t& buffer_size);
-    // std::string lzw_2(const std::string& str, const size_t& buffer_size);
-    // std::string de_lzw_1(const std::string& str, const size_t& buffer_size);
-    // std::string de_lzw_2(const std::string& str, const size_t& buffer_size);
+    sstrtobb lzw_0(const std::string& str, std::unordered_map<std::string, size_t> m_slovar);
+    std::string de_lzw_0(sstrtobb& ssbb, std::vector<std::string> v_slovar);
+    std::string lzw_1(const std::string& str);
+    // std::string lzw_2(const std::string& str);
+    // std::string de_lzw_1(const std::string& str);
+    // std::string de_lzw_2(const std::string& str);
 }
 
-void lzw(std::istream& stream_in, std::ostream& stream_out, const size_t& buffer_size);
-void de_lzw(std::istream& stream_in, std::ostream& stream_out, const size_t& buffer_size);
+void lzw(std::istream& stream_in, std::ostream& stream_out);
+void de_lzw(std::istream& stream_in, std::ostream& stream_out);
