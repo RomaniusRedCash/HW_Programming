1b232160f242dd7c4d81b6481e34a1554179735e
RomaniusRedCash <romaniusredcash@PMC_Roman_Void.localdomain>
2026-07-15 03:04:28 +0300

    Орбиту не надо, кек

--- Programming/2/practice/1/src/main_window.c
+++ Programming/2/practice/1/src/main_window.c
@@ -9,13 +9,11 @@ struct _MainWindow {
     GtkApplicationWindow parent_instance;
     GtkWidget* drawing_area;
     GtkSwitch* napr_switcher;
-    GtkSwitch* orb_switcher;
 
 #define SCALE_LIST \
     X(a_param)      \
     X(diag1)     \
     X(diag2)  \
-    X(speed_orb) \
     X(speed_trajectory) \
     X(ampl) \
     X(speed_ampl) \
@@ -39,7 +37,6 @@ struct _MainWindow {
     int16_t cur_deg;
     double cur_apml;
     gboolean obratno;
-    gboolean orb;
 };
 
 G_DEFINE_TYPE(MainWindow, main_window, GTK_TYPE_APPLICATION_WINDOW)
@@ -47,12 +44,9 @@ G_DEFINE_TYPE(MainWindow, main_window, GTK_TYPE_APPLICATION_WINDOW)
 static void parce_param(gpointer user_data) {
     MainWindow* self = (MainWindow*)user_data;
     self->obratno = gtk_switch_get_state(self->napr_switcher);
-    self->orb = gtk_switch_get_state(self->orb_switcher);
-
 #define X(name) self->name = gtk_range_get_value(GTK_RANGE(self->name##_scale));
     SCALE_LIST
 #undef X
-
 #define X(name) self->color_##name = gtk_color_dialog_button_get_rgba(self->color_##name##_button);
     COLOR_LIST
 #undef X
@@ -99,76 +93,11 @@ static void on_drawing_area_draw(GtkDrawingArea *drawing_area, cairo_t *cr, int 
         else cairo_line_to(cr, x, y);
     }
     cairo_stroke(cr);
-
-    if (self->orb) {
-// ТРАЕКТОРИЯ
-        phi = self->cur_deg * G_PI / 180.0 * self->speed_trajectory;
-        r = self->a_param * sin(3.0 * phi);
-        x = r * sin(phi);
-        y = r * cos(phi);
-        cairo_translate(cr, x, y);
-
-        set_color(cr, color_sub_contur);
-        cairo_set_line_width(cr, 1.5);
-        cairo_arc(cr, 0, 0, 5, 0, 2 * G_PI);
-        cairo_fill(cr);
-// ОРБИТА
-        phi = self->cur_deg * G_PI / 180.0 * self->speed_orb;
-        r = self->a_param / 2.0;
-        x = r * sin(phi);
-        y = r * cos(phi);
-        set_color(cr, color_sub_contur);
-        cairo_set_line_width(cr, 1.5);
-        cairo_set_dash(cr, dash_pattern, num_dashes, 0.0);
-        cairo_arc(cr, 0, 0, r, 0, 2 * G_PI);
-        cairo_stroke(cr);
-        cairo_set_dash(cr, NULL, 0, 0.0);
-
-        cairo_translate(cr, x, y);
-        cairo_rotate(cr, -phi);
         if (self->ampl != 0.0) {
             double rad = self->cur_deg * G_PI * self->speed_ampl / 180.0;
             self->cur_apml = self->ampl * sin(rad);
         } else self->cur_apml = 0.0;
         double scale = 1.0 + self->cur_apml;
-        cairo_scale(cr, scale, scale);
-        cairo_set_line_width(cr, self->obvodka);
-        double sub_diag1 = self->diag1 / 2.0;
-        double sub_diag2 = self->diag2 / 2.0;
-        cairo_move_to(cr, sub_diag1, 0);
-        cairo_line_to(cr, 0, sub_diag2);
-        cairo_line_to(cr, -sub_diag1, 0);
-        cairo_line_to(cr, 0, -sub_diag2);
-        cairo_line_to(cr, sub_diag1, 0);
-        set_color(cr, color_zalivka);
-        cairo_fill_preserve(cr);
-        set_color(cr, color_contur);
-        cairo_stroke(cr);
-    } else {
-// ТРАЕКТОРИЯ ДЛЯ ОРБИТЫ
-        cairo_save(cr);
-        phi = self->cur_deg * G_PI * self->speed_orb / 180.0;
-        r = self->a_param * sin(3.0 * phi);
-        x = r * sin(phi);
-        y = r * cos(phi);
-        cairo_translate(cr, x, y);
-
-        if (self->ampl != 0.0) {
-            double rad = self->cur_deg * G_PI * self->speed_ampl / 180.0;
-            self->cur_apml = self->ampl * sin(rad);
-        } else self->cur_apml = 0.0;
-        double scale = 1.0 + self->cur_apml;
-        cairo_scale(cr, scale, scale);
-
-        cairo_set_line_width(cr, self->obvodka);
-        cairo_arc(cr, 0, 0, self->a_param / 2.0, 0, 2 * G_PI);
-        set_color(cr, color_zalivka);
-        cairo_fill_preserve(cr);
-        set_color(cr, color_contur);
-        cairo_stroke(cr);
-        cairo_restore(cr);
-
-// ТРАЕКТОРИЯ ДЛЯ РОМБА
         phi = self->cur_deg * G_PI * self->speed_trajectory / 180.0;
         r = self->a_param * sin(3.0 * phi);
         x = r * sin(phi);
@@ -196,8 +125,6 @@ static void on_drawing_area_draw(GtkDrawingArea *drawing_area, cairo_t *cr, int 
         set_color(cr, color_contur);
         cairo_stroke(cr);
         cairo_restore(cr);
-    }
-
     cairo_restore(cr);
 #undef set_color
 }
@@ -216,12 +143,9 @@ static void main_window_class_init(MainWindowClass* klass) {
     gtk_widget_class_set_template_from_resource(widget_class, DEFAULT_UI_PATH(builder.ui));
     gtk_widget_class_bind_template_child(widget_class, MainWindow, drawing_area);
     gtk_widget_class_bind_template_child(widget_class, MainWindow, napr_switcher);
-    gtk_widget_class_bind_template_child(widget_class, MainWindow, orb_switcher);
-
 #define X(name) gtk_widget_class_bind_template_child(widget_class, MainWindow, name##_scale);
     SCALE_LIST
 #undef X
-
 #define X(name) gtk_widget_class_bind_template_child(widget_class, MainWindow, color_##name##_button);
     COLOR_LIST
 #undef X
