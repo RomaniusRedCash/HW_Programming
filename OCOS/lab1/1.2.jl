ec8e226d7c7248385c5a6b03c4f47ccb7740b4a1
RomaniusRedCash <>
2026-09-24 09:21:31 +0300

    fix

--- OCOS/lab1/1.2.jl
+++ OCOS/lab1/1.2.jl
@@ -13,7 +13,7 @@ function Zad12()
     _t=-1:0.01:4
     x_q(n,f_s,U_min,U_max,M,x_A)=x_D_z(n,f_s,U_min,U_max,M,x_A)*(U_max - U_min)/2^M # 2
 
-    for i in F
+    for (n,i) in enumerate(F)
         n_range = Int(_t[1]*i[1]):Int(_t[end]*i[1])
         t_vals = n_range ./ i[1]
         x_t(t)=t
@@ -23,7 +23,7 @@ function Zad12()
         e_q_t = t_vals .- x_q_t
         plot!(t_vals, e_q_t, label = "Ошибка")
         if i[end] < 5 hline!([k * dU(i[2:4]...) for k in 0:(2^i[end])], color=:gray, linestyle=:dot, label="Уровни квантования") end
-        if saveZad == "y" savefig(p, "1.2.svg")
+        if saveZad == "y" savefig(p, "1.2,F_$n.svg")
         else
             display(p)
             readline()
